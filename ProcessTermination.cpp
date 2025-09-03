a387fd63f23a4d71f7a4d2fce8f054ad7d84984e
Ishtiak Rahman <ishtiakrahman13579@gmail.com>
2025-09-03 08:07:06 +0600

    Process Termination

---
+++ ProcessTermination.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define optimize() ios_base:: sync_with_stdio (0) ; cin.tie (0) ; cout.tie (0) ;
+
+int main()
+{
+    optimize();
+    cout <<"Hello";
+    return 0;
+}
