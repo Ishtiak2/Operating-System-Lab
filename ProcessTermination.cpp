47dae36fb8f388b6eac690b7bcbc6060650e0169
Ishtiak Rahman <ishtiakrahman13579@gmail.com>
2025-09-03 08:34:53 +0600

    Process Termination updated

--- ProcessTermination.cpp
+++ ProcessTermination.cpp
@@ -1,13 +1,36 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
 
-#define ll long long
-#define endl '\n'
-#define optimize() ios_base:: sync_with_stdio (0) ; cin.tie (0) ; cout.tie (0) ;
+int main(){
 
-int main()
-{
-    optimize();
-    cout <<"Hello";
-    return 0;
-}
+    pid_t pid = fork();
+
+    if(pid==0){
+        printf("Child : My pid is %d\n",getpid());
+        sleep(10);
+        
+        printf("Child : I am exiting now \n");
+        
+        exit(42);
+    }
+    
+    else if(pid > 0) {
+        int status;
+        
+        wait(&status);
+        
+        printf("Parent: Child exited with status %d\n",WEXITSTATUS(status));
+    }
+
+ }
+
+ /*
+ sleep(2); → pauses the current process for 2 seconds. During this time, the CPU can execute other processes.
+ exit(42) -> The child process terminates and returns 42 to the parent.
+ 
+ wait(&status) -> Parent is blocked (paused) until child finishes. Its exit code or termination reason is stored in status variable.
+ WEXITSTATUS(status): “Get the exit code of the child from the status returned by wait()"
+ */
