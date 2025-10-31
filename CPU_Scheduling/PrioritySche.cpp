6c49478d818d4e67f86cc4ceada8aee540f937cb
Ishtiak Rahman <ishtiakrahman13579@gmail.com>
2025-10-31 10:54:25 +0600

    update

--- CPU_Scheduling/PrioritySche.cpp
+++ CPU_Scheduling/PrioritySche.cpp
@@ -4,25 +4,25 @@ using namespace std;
 
 #define MAX 10
 
-void sjf(int n, int bt[], int p[])
+void priorityScheduling(int n, int bt[], int p[], int prio[])
 {
     int wt[MAX], tat[MAX];
 
-    // Sort by burst time (SJF)
+    // Sort by priority (lower number = higher priority)
     for (int i = 1; i <= n - 1; i++)
     {
-        for (int j = i+1; j <= n; j++)
+        for (int j = i + 1; j <= n; j++)
         {
-            if (bt[i] > bt[j])
+            if (prio[i] > prio[j])
             {
+                swap(prio[i], prio[j]);
                 swap(bt[i], bt[j]);
                 swap(p[i], p[j]);
             }
         }
     }
 
-
-    cout << "\nPR\tBT\tWT\tTAT\n";
+    cout << "\nPR\tBT\tPriority\tWT\tTAT\n";
 
     for (int i = 1; i <= n; i++)
     {
@@ -35,15 +35,14 @@ void sjf(int n, int bt[], int p[])
         }
 
         tat[i] = wt[i] + bt[i];
-        
-        cout << p[i] << "\t" << bt[i] << "\t" << wt[i] << "\t" << tat[i] << endl;
-    }
 
+        cout << p[i] << "\t" << bt[i] << "\t" << prio[i] << "\t\t" << wt[i] << "\t" << tat[i] << endl;
+    }
 }
 
 int main()
 {
-    int n, bt[MAX], p[MAX];
+    int n, bt[MAX], p[MAX], prio[MAX];
 
     cout << "Enter number of processes: ";
     cin >> n;
@@ -56,7 +55,14 @@ int main()
         p[i] = i; // process IDs
     }
 
-    sjf(n, bt, p);
+    cout << "Enter Priorities (lower number = higher priority):\n";
+    for (int i = 1; i <= n; i++)
+    {
+        cout << "Process " << i << ": ";
+        cin >> prio[i];
+    }
+
+    priorityScheduling(n, bt, p, prio);
 
     return 0;
 }
